a5d7e90058abbec42127f837dc05ae327a1c4502
anuk549 <aindeepa59@gmail.com>
2025-03-13 16:36:36 +0530

    v2

--- app.py
+++ app.py
@@ -28,8 +28,15 @@ def index():
 @app.route('/add-data', methods=['GET'])
 def add_data_get():
     message = session.get('message', '')
+    
+    labels = np.load('data/labels.npy')
+    count = {k: 0 for k in ENCODER.keys()}
+    for label in labels:
+        count[label] += 1
+    count = sorted(count.items(), key=lambda x: x[1])
+    letter = count[0][0]
 
-    letter = choice(list(ENCODER.keys()))
+    # letter = choice(list(ENCODER.keys()))
 
     return render_template("addData.html", letter=letter, message=message)
 
